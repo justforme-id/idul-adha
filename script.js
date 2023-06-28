8df2847e802b77b598219e41ae952f877a876bae
justforme-id <133552783+justforme-id@users.noreply.github.com>
2023-06-28 12:19:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const Iya = document.querySelector(".next");
 const Text = document.getElementById("text");
 const Image = document.querySelector("#image-gif");
-const audio = new Audio('https://file.io/VjS70ABvXaSR'); // Ganti 'nama_file_lagu.mp3' dengan URL file lagu yang ingin Anda gunakan
+const audio = new Audio('https://srv.neoxr.tk/f/dMvjwB.mp3'); // Ganti 'nama_file_lagu.mp3' dengan URL file lagu yang ingin Anda gunakan!
 
 Iya.addEventListener("click", function () {
     Iya.value = parseInt(Iya.value) + 1;
@@ -10,34 +10,34 @@ Iya.addEventListener("click", function () {
     if (Iya.value == 1) {
         Text.classList.remove('text-animate');
         void Text.offsetWidth; 
-        Text.innerHTML = "Assalamualaikum, Hai ini saya.";
+        Text.innerHTML = "Assalamualaikum, Hai ini saya Amar dan Segenap keluarga besar.";
         Text.classList.add('text-animate');
         audio.play(); // Memulai pemutaran lagu
     } else if (Iya.value == 2) {
         Text.classList.remove('text-animate');
         void Text.offsetWidth; 
-        Text.innerHTML = "Saya mohon maaf jika ada salah";
+        Text.innerHTML = "Kami mohon maaf jika ada salah";
         Image.setAttribute("src", "https://c.tenor.com/uygoMR-ATWgAAAAj/happy-ied-mubarak-ied-mubarak.gif");
         Text.classList.add('text-animate');
     } else if (Iya.value == 3) {
         Text.classList.remove('text-animate');
         void Text.offsetWidth; 
-        Text.innerHTML = "Berbuat khilaf adalah sifat";
+        Text.innerHTML = "Mungkin kalimat ini tidak seindah zikir subuhmu";
         Text.classList.add('text-animate');
     } else if (Iya.value == 4) {
         Text.classList.remove('text-animate');
         void Text.offsetWidth; 
-        Text.innerHTML = "Meminta maaf adalah kewajiban";
+        Text.innerHTML = "Tidak pula sesyahdu doa di sepertiga malammu";
         Text.classList.add('text-animate');
     } else if (Iya.value == 5) {
         Text.classList.remove('text-animate');
         void Text.offsetWidth; 
-        Text.innerHTML = "Dan kembalinya fitrah adalah tujuan";
+        Text.innerHTML = "Begitu juga tak semerdu solawat sebelum fajar terbenam. Namun, doa dan ucapan ini setulus sujud di setiap rakaat salat kita";
         Text.classList.add('text-animate');
     } else if (Iya.value == 6) {
         Text.classList.remove('text-animate');
         void Text.offsetWidth; 
-        Text.innerHTML = "Di hari raya ini mohon maaf lahir dan batin";
+        Text.innerHTML = "Di hari yang suci ini saya ingin memohon maaf lahir dan batin, Semoga Allah membanjiri hidup kita dengan kesehatan, hati dengan cinta, jiwa dengan keimanan, dan pikiran dengan kebahagiaan. Selamat Idul Adha.";
         Text.classList.add('text-animate');
     } else if (Iya.value == 7) {
         Image.setAttribute("src", "https://c.tenor.com/T_16HTs34x4AAAAj/taqabbal-allahu-minna-wa-minkum-accept-god-with-us-and-you.gif");
@@ -50,6 +50,7 @@ Iya.addEventListener("click", function () {
         Text.classList.remove('text-animate');
         void Text.offsetWidth; 
         Text.innerHTML = "Selamat Hari Raya Idul Adha 1444 Hijriah";
+        Text.innerHTML += '<br><a href="https://wa.me/6289510815106" target="_blank" rel="noopener noreferrer"><button>Kirim Pesan</button></a>';
         Text.classList.add('text-animate');
         Iya.remove();
         audio.pause(); // Menghentikan pemutaran lagu
